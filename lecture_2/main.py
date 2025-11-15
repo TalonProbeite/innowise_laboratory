b1ffd5ed98b6832e6a52974f1ad913f1c1b9c8fa
artem ponomarcev <siionunstoppable@gmail.com>
2025-11-15 14:49:27 +0300

    refactor: Add docstrings and fix spelling errors

--- lecture_2/main.py
+++ lecture_2/main.py
@@ -1,4 +1,5 @@
 def generate_profile(age:int)->str:
+    """Returns the life stage (Child, Teenager, Adult) based on age."""
     if age >= 0 and age <= 12:
         return "Child"
     elif age >= 13 and age <=19:
@@ -9,6 +10,7 @@ def generate_profile(age:int)->str:
     
 
 def get_profile_info()->dict:
+    """Collects user information (name, birth year, hobbies) and calculates age and life stage."""
     print("Hello!")
     user_name = input("Enter your full name: ")
     birth_year_str = input("Enter your birth year: ")
@@ -19,7 +21,7 @@ def get_profile_info()->dict:
 
     hobbies = []
     while True:
-        hobby = input("Enter а favorite hobby ог type 'stop' to finish: ")
+        hobby = input("Enter a favorite hobby оr type 'stop' to finish: ")
         if hobby.strip().lower() == "stop":
             break
         hobbies.append(hobby)
@@ -30,26 +32,28 @@ def get_profile_info()->dict:
 
 
 def show_profile(user_profile:dict)->None:
+    """Prints a summary of the user's collected profile data."""
     num_hobbies = len(user_profile["hobbies"])
     if num_hobbies == 0:
-        hobbies_summary = "\nУоu didn't mention апу hobbies.\n"
+        hobbies_summary = "\nYоu didn't mention аnу hobbies.\n"
     else:
         hobbies_summary = f"\nFavorite Hobbies ({num_hobbies}):\n"
         for h in user_profile["hobbies"]:
             hobbies_summary += f"- {h}\n"
         
     
-    summery_profil = f"""---
+    summery_profile = f"""---
 Profil Summary
 Name: {user_profile["name"]}
 Age: {user_profile["age"]}
 Life Stage: {user_profile["stage"]}"""
-    summery_profil += hobbies_summary + "---"
+    summery_profile += hobbies_summary + "---"
 
-    print(summery_profil)
+    print(summery_profile)
 
 
 def main():
+    """Main function to run the profile generation and display process."""
     show_profile(get_profile_info())
 
 
