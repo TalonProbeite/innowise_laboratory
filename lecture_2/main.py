8855f3665144a93a51e52edab87eecf77b88e18b
artem ponomarcev <siionunstoppable@gmail.com>
2025-11-15 14:11:43 +0300

    feat: add get_profile_info

--- lecture_2/main.py
+++ lecture_2/main.py
@@ -5,3 +5,25 @@ def generate_profile(age:int)->str:
         return "Teenager"
     elif age >= 20:
         return "Adult"
+    return "incorrect year of birth"
+    
+
+def get_profile_info()->dict:
+    print("Hello!")
+    user_name = input("Enter your full name: ")
+    birth_year_str = input("Enter your birth year: ")
+    birth_year = int(birth_year_str)
+    current_age = 2025 - birth_year
+    life_stage = generate_profile(current_age)
+
+
+    hobbies = []
+    while True:
+        hobby = input("Enter а favorite hobby ог type 'stop' to finish: ")
+        if hobby.strip().lower() == "stop":
+            break
+        hobbies.append(hobby)
+    
+    user_profile = {"name":user_name , "age":current_age, "stage":life_stage , "hobbies":hobbies}
+
+    return user_profile 
